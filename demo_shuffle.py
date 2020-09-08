00141eaf606e45575a7f9a54128a6c18fc27f34f
Martin Reinecke <martin@mpa-garching.mpg.de>
2020-09-08 16:51:05 +0200

    simple demo script showcasing the proposed queries for guide star candidates

--- demo_shuffle.py
+++ demo_shuffle.py
@@ -1,93 +1,25 @@
 import numpy as np
-
-
-# Translation tables for column names
-coldicts = {
-    "GAIA2" : {
-        "ra" : "ra",
-        "dec" : "dec",
-        "pmra" : "pmra",
-        "pmdec" : "pmdec",
-        "id" : "source_id" },
-    "SDSSdr12" : {
-        "ra" : "RA_ICRS",
-        "dec" : "DE_ICRS",
-        "pmra" : "pmRA",
-        "pmdec" : "pmDE",
-        "id" : "objID" }
-    }
-
-
-def build_circle_query(ra, dec, radius, coldict):
-    return """CONTAINS(POINT('ICRS',{},{}),CIRCLE('ICRS',{},{},{}))=1""".format(coldict["ra"], coldict["dec"], ra, dec, radius)
-
-
-def build_polygon_query(coords, coldct):
-    res = """CONTAINS(POINT('ICRS',{},{}),
-          POLYGON('ICRS',""".format(coldict["ra"], coldict["dec"])
-    for i in range(coords.shape[0]-1):
-        res += str(coords[i,0]) + "," + str(coords[i,1]) + ","
-    res += str(coords[-1,0]) + "," + str(coords[-1,1])
-    res += "))=1"
-    return res
-
-
-def build_mag_query(minmag, maxmag, mag_col):
-    return """{} between {} and {}""".format(mag_col, maxmag, minmag)
-
-
-def build_pm_query(coldict):
-    return """{} is not null and {} is not null""".format(coldict["pmra"], coldict["pmdec"])
-
-
-def openGAIA2connection():
-    from astroquery.gaia import Gaia
-    return Gaia, "gaiadr2.gaia_source", coldicts["GAIA2"]
-
-
-def openVizierSDSSdr12connection():
-    from astroquery.utils.tap.core import TapPlus
-    tap = TapPlus(url="http://TAPVizieR.u-strasbg.fr/TAPVizieR/tap") 
-    return tap, 'vizls."V/147/sdss12"', coldicts["SDSSdr12"]
-
-
-def openVizierGAIA2connection():
-    from astroquery.utils.tap.core import TapPlus
-    tap = TapPlus(url="http://TAPVizieR.u-strasbg.fr/TAPVizieR/tap") 
-    return tap, 'vizls."I/345/gaia2"', coldicts["GAIA2"]
-
-
-def run_query(conn, table, req_columns, constraints):
-    colstring = ""
-    for col in req_columns[:-1]:
-        colstring += col+', '
-    colstring += req_columns[-1]
-    constr = ""
-    for constraint in constraints[:-1]:
-        constr += constraint+' AND '
-    constr += constraints[-1]
-    query_string = 'SELECT ' + colstring + ' FROM ' + table + ' WHERE ' + constr + ';'
-    print(query_string)
-    job = conn.launch_job_async(query_string, dump_to_file=False)
-    res = job.get_results()
-    return res
+from coordinates.CoordTransp import CoordinateTransform as ctrans
+from ets_shuffle import query_utils
+import matplotlib.path as mppath
 
 
 def flag_close_pairs(ra, dec, radius):
     ra, dec = ra*np.pi/180, dec*np.pi/180
     rsq = (radius*np.pi/180)**2
-    assert(ra.ndim==1 and dec.ndim==1 and ra.shape[0]==dec.shape[0], "bad input")
+    assert ra.ndim == 1 and dec.ndim == 1 and ra.shape[0] == dec.shape[0],\
+        "bad input"
     nval = ra.shape[0]
-    xyz = np.zeros((nval,3), dtype=np.float64)
+    xyz = np.zeros((nval, 3), dtype=np.float64)
     cdec = np.cos(dec)
     xyz[..., 0] = cdec*np.cos(ra)
     xyz[..., 1] = cdec*np.sin(ra)
     xyz[..., 2] = np.sin(dec)
     flags = np.zeros(nval, dtype=np.bool)
     for i in range(ra.shape[0]):
-        distsq = ((xyz[i,0]-xyz[i+1:,0])**2
-                 +(xyz[i,1]-xyz[i+1:,1])**2
-                 +(xyz[i,2]-xyz[i+1:,2])**2)
+        distsq = ((xyz[i, 0]-xyz[i+1:, 0])**2
+                  + (xyz[i, 1]-xyz[i+1:, 1])**2
+                  + (xyz[i, 2]-xyz[i+1:, 2])**2)
         tflags = distsq < rsq
         if np.any(tflags):
             flags[i] = True
@@ -95,69 +27,125 @@ def flag_close_pairs(ra, dec, radius):
     return flags
 
 
-def update_coords_for_proper_motion(ra, dec, pmra, pmdec, epoch_cat, epoch_now):
-    deltaRA = (epoch_now - epoch_cat)*pmra / 3600000. / np.cos(dec*np.pi/180.)
-    deltaDE = (epoch_now - epoch_cat)*pmdec / 3600000.
-    return ra+deltaRA, dec + deltaDE
+def update_coords_for_proper_motion(ra, dec, pmra, pmdec, epoch_cat,
+                                    epoch_now):
+    deltaRA = (epoch_now - epoch_cat)*pmra/3600000./np.cos(dec*np.pi/180.)
+    deltaDE = (epoch_now - epoch_cat)*pmdec/3600000.
+    return ra+deltaRA, dec+deltaDE
 
 
-if __name__ == "__main__":
+def plot_focal_plane(cameras, targets, gstargets):
+    import matplotlib.pyplot as plt
+    import matplotlib.path as mppath
+    import matplotlib.patches as patches
+    fig, ax = plt.subplots()
+    for i in range(cameras.shape[0]):
+        p = mppath.Path(cameras[i, (0, 1, 2, 3, 0), :], closed=True)
+        patch = patches.PathPatch(p, fill=False)
+        ax.add_patch(patch)
+    ax.set_xlim(-300, 300)
+    ax.set_ylim(-300, 300)
+    ax.scatter(targets[:, 0], targets[:, 1], color='g', marker=".")
+    ax.scatter(gstargets[:, 0], gstargets[:, 1], color='r')
+    plt.show()
+
+
+# returns a (6,4,2) float array with PFI coordinates of the 4 corners
+# of the 6 guide star cams.
+# This is a placeholder until we know how to obtain the real geometry
+def guidecam_geometry():
+    # The AG camera is described by its 4 corners. Let's use a square with
+    # 10 mm side length for now, at 25mm distance from the center
+    agcoord0 = np.zeros((4, 2))
+    dist = 250.
+    sidelength = 10.
+    agcoord0[0, :] = [-.5*sidelength, -.5*sidelength]
+    agcoord0[1, :] = [+.5*sidelength, -.5*sidelength]
+    agcoord0[2, :] = [+.5*sidelength, +.5*sidelength]
+    agcoord0[3, :] = [-.5*sidelength, +.5*sidelength]
+    agcoord0[:, 0] += dist
+    # get the remaining cameras via 60-degree rotations
+    agcoord = np.zeros((6, 4, 2))
+    for i in range(0, 6):
+        ang = (30. + 60.*i)*np.pi/180.
+        ca, sa = np.cos(ang), np.sin(ang)
+        for j in range(4):
+            x, y = agcoord0[j, :]
+            agcoord[i, j, :] = ca*x - sa*y, ca*y + sa*x
+    return agcoord
+
+
+# radius of the focal plane: roughly 220mm
+# 1mm corresponds roughly to 10.2 arcseconds
+
+def main():
+    # Section with externally provided parameters
+
+    obs_time = "2020-01-01 15:00:00"
+    # telescope pointing
+    raTel_deg, decTel_deg = 34., -3.
+    # focal plane position angle
+    pa_deg = 0.
+    # maximum magnitude for guide stars
+    guidestar_mag_max = 20
+    # guide star cam geometries
+    agcoord = guidecam_geometry()
+
+    # internal, technical parameters
     # set focal plane radius
-    fp_rad_deg = 1.
-    # set some telescope pointing
-    raTel_deg, decTel_deg = 34, -3
-
-
-    # Find bright stars within FOV
-
-    # Query GAIA via their own server
-    conn, table, coldict = openGAIA2connection()
-    req_columns = [coldict["id"], coldict["ra"], coldict["dec"], coldict["pmra"], coldict["pmdec"], 'phot_g_mean_mag']
-    constraints = [build_circle_query(raTel_deg, decTel_deg, fp_rad_deg, coldict),
-                   build_pm_query(coldict),
-                   build_mag_query(15, 0, 'phot_g_mean_mag')]
-    res = run_query(conn, table, req_columns, constraints)
-    print(res)
-
-    # Query Vizier's copy of GAIA
-    conn, table, coldict = openVizierGAIA2connection()
-    req_columns = [coldict["id"], coldict["ra"], coldict["dec"], coldict["pmra"], coldict["pmdec"], 'phot_g_mean_mag']
-    constraints = [build_circle_query(raTel_deg, decTel_deg, fp_rad_deg, coldict),
-                   build_pm_query(coldict),
-                   build_mag_query(15, 0, 'phot_g_mean_mag')]
-    res = run_query(conn, table, req_columns, constraints)
-    print(res)
-
-    # Query Vizier's copy of SDSS
-    conn, table, coldict = openVizierSDSSdr12connection()
-    req_columns = [coldict["id"], coldict["ra"], coldict["dec"], coldict["pmra"], coldict["pmdec"], 'gmag']
-    constraints = [build_circle_query(raTel_deg, decTel_deg, fp_rad_deg, coldict),
-                   build_pm_query(coldict),
-                   build_mag_query(15, 0, 'gmag')]
-    res = run_query(conn, table, req_columns, constraints)
-    print(res)
-
+    fp_rad_deg = 250. * 10.2/3600
 
     # Find guide star candidates
 
-    # Query GAIA2 for a rectangular region
-    conn, table, coldict = openGAIA2connection()
-    req_columns = [coldict["id"], coldict["ra"], coldict["dec"], coldict["pmra"], coldict["pmdec"], 'phot_g_mean_mag']
-    # The AG camera is described by its 4 corners. Let's use a square with
-    # 2.5 arcmin side length for now
-    # I'm also putting it into the center of the FP because it's just an example
-    agcoord = np.zeros((4,2))
-    sidelength = 2.5/60
-    agcoord[0,:] = [raTel_deg, decTel_deg]
-    agcoord[1,:] = [raTel_deg+sidelength, decTel_deg]
-    agcoord[2,:] = [raTel_deg+sidelength, decTel_deg+sidelength]
-    agcoord[3,:] = [raTel_deg, decTel_deg+sidelength]
-    constraints = [build_polygon_query(agcoord, coldict),
-                   build_pm_query(coldict),
-                   build_mag_query(20, 0, 'phot_g_mean_mag')]
-    res = run_query(conn, table, req_columns, constraints)
-    flags = flag_close_pairs(res[coldict["ra"]], res[coldict["dec"]], 0.0000001)
-    ranew, decnew = update_coords_for_proper_motion(res[coldict["ra"]], res[coldict["dec"]], res[coldict["pmra"]], res[coldict["pmdec"]], 2000., 2020.)
-    print(decnew-res[coldict["dec"]])
-    print(res)
-    print(flags)
+    # Query GAIA2 for a circular region containing all guide cam FOVs
+    # Obtain all targets with g_mean_mag<=guidestar_mag_max that have
+    # proper motion information
+    conn, table, coldict = query_utils.openGAIA2connection()
+    racol, deccol = coldict["ra"], coldict["dec"]
+    req_columns = [coldict["id"], racol, deccol, coldict["pmra"],
+                   coldict["pmdec"], 'phot_g_mean_mag']
+    constraints = [
+        query_utils.build_circle_query(
+            raTel_deg, decTel_deg, fp_rad_deg*1.2, coldict),
+        query_utils.build_pm_query(coldict),
+        query_utils.build_mag_query(guidestar_mag_max, 0, 'phot_g_mean_mag')]
+    res = query_utils.run_query(conn, table, req_columns, constraints)
+    nfound = res[racol].size
+    # adjust for proper motion
+    obs_year = float(obs_time[0:4])
+    res[racol], res[deccol] = \
+        update_coords_for_proper_motion(res[racol], res[deccol],
+                                        res[coldict["pmra"]],
+                                        res[coldict["pmdec"]], 2000., obs_year)
+
+    # compute PFI coordinates
+    tmp = np.array([res[racol], res[deccol]])
+    tmp = ctrans(xyin=tmp,
+                 za=0., mode="sky_pfi", inr=0., pa=pa_deg,
+                 cent=np.array([raTel_deg, decTel_deg]),
+                 time=obs_time)
+    res["xypos"] = np.array([tmp[0, :], tmp[1, :]]).T
+
+    # determine the subset of sources falling within the guide cam FOVs
+    # For the moment I'm using matplotlib's path functionality for this task
+    # Once the "pfi_sky" transformation direction is available in
+    # pfs_utils.coordinates, we can do a direct polygon query for every camera,
+    # which should be more efficient.
+    targets = np.zeros((0, 2))
+    for i in range(agcoord.shape[0]):
+        p = mppath.Path(agcoord[i])
+        tmp = p.contains_points(res["xypos"])
+        tdict = {}
+        for key, val in res.items():
+            tdict[key] = val[tmp]
+        # eliminate close neighbors
+        flags = flag_close_pairs(tdict[racol], tdict[deccol], 0.0000001)
+        for key, val in tdict.items():
+            tdict[key] = val[np.invert(flags)]
+        targets = np.concatenate((targets, tdict["xypos"]))
+
+    plot_focal_plane(agcoord, res["xypos"], targets)
+
+
+if __name__ == "__main__":
+    main()
