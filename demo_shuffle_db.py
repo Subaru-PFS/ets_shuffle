19bdf12ff46819bd90d677252af26ce9e36cfa02
Martin Reinecke <martin@mpa-garching.mpg.de>
2020-10-29 14:19:02 +0100

    factor out common code and update AG camera geometry

--- demo_shuffle_db.py
+++ demo_shuffle_db.py
@@ -1,6 +1,10 @@
 import numpy as np
 from pfs.utils.coordinates.CoordTransp import CoordinateTransform as ctrans
 from ets_shuffle import query_utils
+from ets_shuffle.convenience import (flag_close_pairs,
+                                     update_coords_for_proper_motion,
+                                     plot_focal_plane,
+                                     guidecam_geometry)
 import matplotlib.path as mppath
 
 
@@ -17,81 +21,7 @@ def get_db():
     return db
 
 
-
-def flag_close_pairs(ra, dec, radius):
-    ra, dec = ra*np.pi/180, dec*np.pi/180
-    rsq = (radius*np.pi/180)**2
-    assert ra.ndim == 1 and dec.ndim == 1 and ra.shape[0] == dec.shape[0],\
-        "bad input"
-    nval = ra.shape[0]
-    xyz = np.zeros((nval, 3), dtype=np.float64)
-    cdec = np.cos(dec)
-    xyz[..., 0] = cdec*np.cos(ra)
-    xyz[..., 1] = cdec*np.sin(ra)
-    xyz[..., 2] = np.sin(dec)
-    flags = np.zeros(nval, dtype=np.bool)
-
-    for i in range(ra.shape[0]):
-        distsq = np.sum((xyz[i]-xyz[i+1:])**2, axis=-1)
-        tflags = distsq < rsq
-        if np.any(tflags):
-            flags[i] = True
-        flags[i+1:] = np.logical_or(flags[i+1:], tflags)
-    return flags
-
-
-def update_coords_for_proper_motion(ra, dec, pmra, pmdec, epoch_cat,
-                                    epoch_now):
-    deltaRA = (epoch_now - epoch_cat)*pmra/3600000./np.cos(dec*np.pi/180.)
-    deltaDE = (epoch_now - epoch_cat)*pmdec/3600000.
-    return ra+deltaRA, dec+deltaDE
-
-
-def plot_focal_plane(cameras, targets, gstargets):
-    import matplotlib.pyplot as plt
-    import matplotlib.path as mppath
-    import matplotlib.patches as patches
-    fig, ax = plt.subplots()
-    for i in range(cameras.shape[0]):
-        p = mppath.Path(cameras[i, (0, 1, 2, 3, 0), :], closed=True)
-        patch = patches.PathPatch(p, fill=False)
-        ax.add_patch(patch)
-    ax.set_xlim(-300, 300)
-    ax.set_ylim(-300, 300)
-    ax.scatter(targets[:, 0], targets[:, 1], color='g', marker=".")
-    ax.scatter(gstargets[:, 0], gstargets[:, 1], color='r')
-    plt.show()
-
-
-# returns a (6,4,2) float array with PFI coordinates of the 4 corners
-# of the 6 guide star cams.
-# This is a placeholder until we know how to obtain the real geometry
-# rough values are taken from
-# https://sumire.pbworks.com/w/file/58576912/agcamerastudy.pdf
-def guidecam_geometry():
-    # The AG camera is described by its 4 corners. Let's use a square with
-    # 10 mm side length for now, at 25mm distance from the center
-    agcoord0 = np.zeros((4, 2))
-    dist = 250.
-    sidelength = 15.
-    agcoord0[0, :] = [-.5*sidelength, -.5*sidelength]
-    agcoord0[1, :] = [+.5*sidelength, -.5*sidelength]
-    agcoord0[2, :] = [+.5*sidelength, +.5*sidelength]
-    agcoord0[3, :] = [-.5*sidelength, +.5*sidelength]
-    agcoord0[:, 0] += dist
-    # get the remaining cameras via 60-degree rotations
-    agcoord = np.zeros((6, 4, 2))
-    for i in range(0, 6):
-        ang = (30. + 60.*i)*np.pi/180.
-        ca, sa = np.cos(ang), np.sin(ang)
-        for j in range(4):
-            x, y = agcoord0[j, :]
-            agcoord[i, j, :] = ca*x - sa*y, ca*y + sa*x
-    return agcoord
-
-
-# radius of the focal plane: roughly 220mm
-# 1mm corresponds roughly to 10.2 arcseconds
+# 1mm on the focal plane corresponds roughly to 10.2 arcseconds
 
 def main():
     # establish DB connection
